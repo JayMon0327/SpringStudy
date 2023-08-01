ea335b1c4eb9a00fdfee5c2d9cc3077b3792956e
JayMon <kioplm0211@gmail.com>
2023-08-02 02:09:38 +0900

    데이터베이스 H2 연동 순수 JDBC JdbcTemplate

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -13,10 +16,10 @@ public class MemberController {
     private final MemberService memberService;
 
     @Autowired
-    public MemberController(MemberService memberService)
-    {
+    public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
     @GetMapping("/members/new")
     public String createForm() {
         return "members/createMemberForm";
@@ -30,6 +33,13 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
 
