37c6d8db9500d07ef65ce8b1834d8d4bec86d765
JayMon0327 <kioplm0211@naver.com>
2023-08-19 00:03:56 +0900

    의존관계 자동 주입 - List,Map/ 자동,수동의 올바른 실무 운영 기준

--- src/test/java/hello/core/xml/XmlAppContext.java
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -1,7 +1,6 @@
 package hello.core.xml;
 
 import hello.core.member.MemberService;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
