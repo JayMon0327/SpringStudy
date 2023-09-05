38f4c5f986686b5056940c15705dbfb3705182eb
JayMon0327 <kioplm0211@naver.com>
2023-09-05 16:42:08 +0900

    스프링 MVC 섹션 6 - 스프링 MVC 기본기능
    HTTP 응답 메시지 - 정적 리소스, 뷰 템플릿
    HTTP 응답 메시지 - HTTP API, 메세지 바디에 직접 입력

---
+++ Spring MVC 2/src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,28 @@
+package hello.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1() {
+        ModelAndView mav = new ModelAndView("response/hello")
+                .addObject("data", "hello!");
+        return mav;
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model) {
+        model.addAttribute("data", "hello!");
+        return "response/hello";
+    }
+
+    @RequestMapping("/response/hello") // 권장 X , RequestMapping 경로와 뷰 이름이 일치하면 리턴값생략가능
+    public void responseViewV3(Model model) {
+        model.addAttribute("data", "hello!");
+    }
+}
