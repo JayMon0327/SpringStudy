d0d4a6cd25c5979c47663909632667791fbbeedd
JayMon0327 <kioplm0211@naver.com>
2023-09-17 22:43:53 +0900

    스프링 부트와 JPA 활용 - 섹션 2 도메인 분석설계 완료
    엔티티 설계시 주의점
    1. 가급적 Setter를 사용하지 말자.
    2. 모든 연관관계는 지연로딩(LAZY)로 설정(x to one)
    3. 컬렉션은 필드에서 초기화 하자
    4. 연관관계 편의 메서드

--- SpringBoot+JPA1/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ SpringBoot+JPA1/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -6,7 +6,10 @@ import lombok.Setter;
 
 import javax.persistence.*;
 
+import static javax.persistence.FetchType.*;
+
 @Entity
+@Table(name = "order_item")
 @Getter
 @Setter
 public class OrderItem {
@@ -16,13 +19,13 @@ public class OrderItem {
     @Column(name = "order_item_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "item_id")
-    private Item item;
+    private Item item; //주문상품
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "order_id")
-    private Order order;
+    private Order order; //주문
 
     private int orderPrice; // 주문 가격
     private int count; //주문 수량
--- SpringBoot+JPA1/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ SpringBoot+JPA1/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,9 +1,14 @@
 package jpabook.jpashop.domain.item;
 
+import jpabook.jpashop.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.FetchType.*;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
@@ -19,5 +24,15 @@ public class Item {
     private String name;
     private int price;
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<Category>();
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
 
 }
