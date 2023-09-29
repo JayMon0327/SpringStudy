adde9a456abe2448d82cc308d6e2bbadf2ea562e
JayMon0327 <kioplm0211@naver.com>
2023-09-29 18:50:40 +0900

    스프링 JPA - 영속성 컨텍스트

--- Spring JPA/jpa-example/src/main/java/hello/jpa/JpaMain.java
+++ Spring JPA/jpa-example/src/main/java/hello/jpa/JpaMain.java
@@ -2,6 +2,7 @@ package hello.jpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
@@ -11,7 +12,32 @@ public class JpaMain {
 
         EntityManager em = emf.createEntityManager();
 
-        em.close();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            //비영속
+//            Member member = new Member();
+//            member.setId(101L);
+//            member.setName("HelloJPA");
+//
+//            //영속
+//            System.out.println("--before--");
+//            em.persist(member);
+//            System.out.println("--after--");
+
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZZ");
+
+            System.out.println("------------------");
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
 
         emf.close();
 
