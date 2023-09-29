a0df7c0d23b84aa55cbc3228e3c0aa96442326d6
JayMon0327 <kioplm0211@naver.com>
2023-09-30 07:22:41 +0900

    스프링 JPA - 엔티티 매핑- 기본 키 매핑

--- Spring JPA/jpa-example/src/main/java/hello/jpa/JpaMain.java
+++ Spring JPA/jpa-example/src/main/java/hello/jpa/JpaMain.java
@@ -17,10 +17,7 @@ public class JpaMain {
 
         try {
             Member member = new Member();
-            member.setId(3L);
             member.setUsername("C");
-            member.setRoleType(RoleType.GUEST);
-
             em.persist(member);
 
             tx.commit();
--- Spring JPA/jpa-example/src/main/java/hello/jpa/Member.java
+++ Spring JPA/jpa-example/src/main/java/hello/jpa/Member.java
@@ -1,35 +1,17 @@
 package hello.jpa;
 
 import javax.persistence.*;
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.Date;
+
 
 @Entity
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name", nullable = false, columnDefinition = "varchar(100) default 'EMPTY'") // 객체는 username을 쓰고싶은데 DB에는 name이야
+    @Column(name = "name", nullable = false) // 객체는 username을 쓰고싶은데 DB에는 name이야
     private String username;
-    private int age;
-
-    @Enumerated(EnumType.STRING) //DB에는 enum 타입이 없기때문에 사용
-    private RoleType roleType;
-
-    @Temporal(TemporalType.TIMESTAMP) //자바에는 날짜, 시간, 날짜와 시간 타입이 있는데 DB에서는 구분에서 써야함
-    private Date createdDate;
-
-    private LocalTime testLocalDate; //연월
-    private LocalDateTime testLocalDateTime; //연월일
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    @Lob //varchar를 넘어서는 큰 문자열을 사용할 때
-    private String description;
 
     public Member() {
     }
@@ -48,45 +30,5 @@ public class Member {
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public RoleType getRoleType() {
-        return roleType;
-    }
-
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 }
