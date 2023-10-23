e4c1835dd5375920df988bc8b6723e5395984adf
JayMon0327 <kioplm0211@naver.com>
2023-10-24 02:45:45 +0900

    MVC 활용 - Validation - Validator 분리 - 검증 로직을 ItemValidator로 분리 - WebDataBinder를 통해서 validator 사용 - @Validated

---
+++ Spring MVC_application/validation/src/test/java/hello/itemservice/validation/BeanValidationTest.java
@@ -0,0 +1,29 @@
+package hello.itemservice.validation;
+
+import hello.itemservice.domain.item.Item;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.*;
+import java.util.Set;
+
+
+public class BeanValidationTest {
+
+    @Test
+    void beanValidation() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+
+        Item item = new Item();
+        item.setItemName(" "); // 공백
+        item.setPrice(0);
+        item.setQuantity(10000);
+
+        Set<ConstraintViolation<Item>> validations = validator.validate(item);
+        for (ConstraintViolation<Item> validation : validations) {
+            System.out.println("validation = " + validation);
+            System.out.println("validation = " + validation.getMessage());
+        }
+
+    }
+}
