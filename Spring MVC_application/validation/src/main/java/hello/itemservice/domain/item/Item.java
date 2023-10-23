867b6cda085cc7796539d29cd2b7a9094305eb0a
JayMon0327 <kioplm0211@naver.com>
2023-10-24 03:13:37 +0900

    MVC 활용 - Bean Validation - ValidationItemControllerV3로 전환 - Bean Validation 라이브러리 추가 - V3 컨트롤러 에서 ItemValidator 참조 삭제 - errors.properties 변경 -> Bean Validation 코드 추가

--- Spring MVC_application/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ Spring MVC_application/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,6 +2,7 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
