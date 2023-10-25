16007bc2c565b5c9ab71c82059235c3e300cb462
JayMon0327 <kioplm0211@naver.com>
2023-10-26 04:14:38 +0900

    MVC 활용 - API 예외 처리 - HandlerExceptionResolver -ExceptionResolver 2개 직접 구현

---
+++ Spring MVC_application/exception/src/main/java/hello/exception/exception/UserException.java
@@ -0,0 +1,20 @@
+package hello.exception.exception;
+
+public class UserException extends RuntimeException{
+
+    public UserException() {
+        super();
+    }
+
+    public UserException(String message) {
+        super(message);
+    }
+
+    public UserException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserException(Throwable cause) {
+        super(cause);
+    }
+}
