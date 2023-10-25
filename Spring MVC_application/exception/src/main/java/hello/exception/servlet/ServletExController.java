3d2a0d247cb457d4c61d857dc55b7ad3680b84dd
JayMon0327 <kioplm0211@naver.com>
2023-10-26 03:45:50 +0900

    MVC 활용 - API 예외 처리 - 스프링 부트 기본 오류 처리

--- Spring MVC_application/exception/src/main/java/hello/exception/servlet/ServletExController.java
+++ Spring MVC_application/exception/src/main/java/hello/exception/servlet/ServletExController.java
@@ -12,19 +12,6 @@ import java.io.IOException;
 @Controller
 public class ServletExController {
 
-    //RequestDispatcher 상수로 정의되어 있음
-    public static final String ERROR_EXCEPTION =
-            "javax.servlet.error.exception";
-    public static final String ERROR_EXCEPTION_TYPE =
-            "javax.servlet.error.exception_type";
-    public static final String ERROR_MESSAGE = "javax.servlet.error.message";
-    public static final String ERROR_REQUEST_URI =
-            "javax.servlet.error.request_uri";
-    public static final String ERROR_SERVLET_NAME =
-            "javax.servlet.error.servlet_name";
-    public static final String ERROR_STATUS_CODE =
-            "javax.servlet.error.status_code";
-
     @GetMapping("/error-ex")
     public void errorEx() {
         throw new RuntimeException("예외 발생!");
@@ -38,21 +25,6 @@ public class ServletExController {
     @GetMapping("/error-500")
     public void error500(HttpServletResponse response) throws IOException {
         response.sendError(500,"500 에러 발생");
-    }
-
-    private void printErrorInfo(HttpServletRequest request) {
-        log.info("ERROR_EXCEPTION: ex=",
-                request.getAttribute(ERROR_EXCEPTION));
-        log.info("ERROR_EXCEPTION_TYPE: {}",
-                request.getAttribute(ERROR_EXCEPTION_TYPE));
-        log.info("ERROR_MESSAGE: {}", request.getAttribute(ERROR_MESSAGE)); // ex의 경우 NestedServletException 스프링이 한번 감싸서 반환
-        log.info("ERROR_REQUEST_URI: {}",
-                request.getAttribute(ERROR_REQUEST_URI));
-        log.info("ERROR_SERVLET_NAME: {}",
-                request.getAttribute(ERROR_SERVLET_NAME));
-        log.info("ERROR_STATUS_CODE: {}",
-                request.getAttribute(ERROR_STATUS_CODE));
-        log.info("dispatchType={}", request.getDispatcherType());
     }
 
 }
