3a71c7850e870cd8bb9a9677a9a2e35fe69f8954
JayMon0327 <kioplm0211@naver.com>
2023-10-26 14:49:33 +0900

    MVC 활용 - API 예외 처리 - 스프링 ExceptionResolver -@ExceptionHandler -ControllerAdvice

--- Spring MVC_application/exception/src/main/java/hello/exception/api/ApiExceptionV3Controller.java
+++ Spring MVC_application/exception/src/main/java/hello/exception/api/ApiExceptionV3Controller.java
@@ -4,13 +4,15 @@ import hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-public class ApiExceptionV2Controller {
+public class ApiExceptionV3Controller {
 
-    @GetMapping("/api2/members/{id}")
+    @GetMapping("/api3/members/{id}")
     public ApiExceptionController.MemberDto getMember(@PathVariable("id") String id) {
 
 
