d2f9bec4377a33dd7677457e48292c36820ad5b6
JayMon0327 <kioplm0211@naver.com>
2023-10-29 01:29:48 +0900

    스프링 DB 이론 - 체크예외와 인터페이스 - 런타임 예외 적용 - 인터페이스 상속 , SQLException 제거

--- Spring DB_Theory/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV3.java
+++ Spring DB_Theory/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV3.java
@@ -15,7 +15,7 @@ import java.util.NoSuchElementException;
  * DataSourceUtils.releaseConnection();
  */
 @Slf4j
-public class MemberRepositoryV3 {
+public class MemberRepositoryV3{
     private final DataSource dataSource;
 
     public MemberRepositoryV3(DataSource dataSource) {
