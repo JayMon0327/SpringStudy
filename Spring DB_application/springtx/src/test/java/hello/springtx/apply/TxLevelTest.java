d3aa4c66770a47ab485146430c728ed9ea38c6eb
JayMon0327 <kioplm0211@naver.com>
2023-10-30 17:23:43 +0900

    DB 활용 - 스프링 트랜잭션 AOP 주의사항 - 프록시 내부 호출을 해결하기 위해 외부 클래스로 메서드 이동 - 초기화 시점에 트랜잭션 미적용을 막기 위해 @EventListener 사용

---
+++ Spring DB_application/springtx/src/test/java/hello/springtx/apply/TxLevelTest.java
@@ -0,0 +1,57 @@
+package hello.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@SpringBootTest
+public class TxLevelTest {
+
+    @Autowired
+    LevelService service;
+
+    @Test
+    void orderTest() {
+        service.write();
+        service.read();
+    }
+
+    @TestConfiguration
+    static class TxLevelTestConfig {
+        @Bean
+        LevelService levelService() {
+            return new LevelService();
+        }
+    }
+
+
+
+    @Slf4j
+    @Transactional(readOnly = true) //읽기 전용 옵션
+    static class LevelService {
+
+        @Transactional(readOnly = false)
+        public void write() {
+            log.info("call write");
+            printTxInfo();
+        }
+
+        public void read() { // 클래스 레벨에서 읽기 전용 옵션 켰으니 자동 적용
+            log.info("call read");
+            printTxInfo();
+        }
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+            boolean readOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+            log.info("tx readOnly={}", readOnly);
+        }
+    }
+
+}
