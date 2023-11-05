e7ceb1348ec3b0b4f67bcaa1ec9cf56035bae174
JayMon0327 <kioplm0211@naver.com>
2023-11-06 02:43:05 +0900

    JPA 활용 1 복습 - 상품 도메인 개발 - 상품 레포지토리 개발 - 상품 서비스 개발 - 상품 엔티티 개발(비즈니스 로직 추가)

--- SpringBoot+JPA1/JPAshop-review/src/main/java/hello/JPAshopreview/domain/item/Item.java
+++ SpringBoot+JPA1/JPAshop-review/src/main/java/hello/JPAshopreview/domain/item/Item.java
@@ -1,6 +1,7 @@
 package hello.JPAshopreview.domain.item;
 
 import hello.JPAshopreview.domain.Category;
+import hello.JPAshopreview.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,6 +25,30 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+
+    // == 비즈니스 로직 ==
+
+    /**
+     * stock 증가(재고증가)
+     * @param quantity
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     * @param quantity
+     */
+    public void removeStock(int quantity) {
+        int result = this.stockQuantity - quantity;
+        if (result < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = result;
+    }
+
 
 }
 
---
+++ SpringBoot+JPA1/JPAshop-review/src/main/java/hello/JPAshopreview/service/ItemService.java
@@ -0,0 +1,30 @@
+package hello.JPAshopreview.service;
+
+import hello.JPAshopreview.domain.item.Item;
+import hello.JPAshopreview.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void save(Item item) {
+        itemRepository.save(item);
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+}
