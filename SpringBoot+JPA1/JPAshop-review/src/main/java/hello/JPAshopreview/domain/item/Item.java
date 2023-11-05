102feecd63e260a44cda0fa8425a4b9d9660c0c0
JayMon0327 <kioplm0211@naver.com>
2023-11-05 20:34:05 +0900

    JPA 활용 1 복습 - 엔티티 클래스 개발 2 - Category 추가 다대다 매핑 - 연관관계 편의 메서드 작성(order, category) - LAZY 설정

--- SpringBoot+JPA1/JPAshop-review/src/main/java/hello/JPAshopreview/domain/item/Item.java
+++ SpringBoot+JPA1/JPAshop-review/src/main/java/hello/JPAshopreview/domain/item/Item.java
@@ -1,9 +1,12 @@
 package hello.JPAshopreview.domain.item;
 
+import hello.JPAshopreview.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter @Setter
@@ -18,6 +21,9 @@ public abstract class Item {
     private String name;
     private int price;
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 
 }
 
