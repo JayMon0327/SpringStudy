6f822736f557cfa3f46bf981eedb4b3049000d95
JayMon0327 <kioplm0211@naver.com>
2023-11-06 14:34:10 +0900

    JPA 활용 1 복습 - 회원 목록 조회 개발

--- SpringBoot+JPA1/JPAshop-review/src/main/java/hello/JPAshopreview/controller/MemberController.java
+++ SpringBoot+JPA1/JPAshop-review/src/main/java/hello/JPAshopreview/controller/MemberController.java
@@ -3,6 +3,7 @@ package hello.JPAshopreview.controller;
 import hello.JPAshopreview.domain.Address;
 import hello.JPAshopreview.domain.Member;
 import hello.JPAshopreview.service.MemberService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @Slf4j
@@ -40,5 +42,12 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
