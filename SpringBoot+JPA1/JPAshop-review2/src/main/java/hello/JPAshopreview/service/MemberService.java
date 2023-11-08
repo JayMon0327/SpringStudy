97e7c87f691723546a98274c20e123df4ef8d8c5
JayMon0327 <kioplm0211@naver.com>
2023-11-09 00:17:48 +0900

    JPA 활용1 복습 2 - 회원 서비스 개발 - 회원 가입 - 중복 회원 검증 - 회원 조회 - 회원 전체 조회

---
+++ SpringBoot+JPA1/JPAshop-review2/src/main/java/hello/JPAshopreview/service/MemberService.java
@@ -0,0 +1,40 @@
+package hello.JPAshopreview.service;
+
+import hello.JPAshopreview.domain.Member;
+import hello.JPAshopreview.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Long Join(Member member) {
+        validationDuplicateMember(member); // 중복회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validationDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원 입니다.");
+        }
+    }
+
+    //회원 전체 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
