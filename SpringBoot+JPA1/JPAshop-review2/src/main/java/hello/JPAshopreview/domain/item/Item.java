c28a15274d9768ac0472d3d71b5de5c20c5a3053
JayMon0327 <kioplm0211@naver.com>
2023-11-09 00:50:35 +0900

    JPA 활용1 복습 2 - 상품 엔티티 비즈니스 로직 작성 - 재고 증가 로직 - 재고 감소 로직 - 상품 수정 로직

--- SpringBoot+JPA1/JPAshop-review2/src/main/java/hello/JPAshopreview/domain/item/Item.java
+++ SpringBoot+JPA1/JPAshop-review2/src/main/java/hello/JPAshopreview/domain/item/Item.java
@@ -1,6 +1,7 @@
 package hello.JPAshopreview.domain.item;
 
 import hello.JPAshopreview.domain.Category;
+import hello.JPAshopreview.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,34 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+
+    //== 비즈니스 로직 ==//
+
+    /**
+     * 재고 증가 로직
+     */
+    public void addStock(int stockQuantity) {
+        this.stockQuantity += stockQuantity;
+    }
+
+    /**
+     * 재고 감소 로직
+     */
+    public void removeStock(int stockQuantity) {
+        int result = this.stockQuantity - stockQuantity;
+        if (result < 0) {
+            throw new NotEnoughStockException("재고가 부족합니다.");
+        }
+        this.stockQuantity = result;
+    }
+
+    /**
+     * 상품 수정
+     */
+    public void change(String name, int price, int stockQuantity) {
+        this.name = name;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
 }
