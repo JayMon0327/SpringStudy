73d453ef3da8813246f7efeda2a805c0f21b4748
JayMon0327 <kioplm0211@naver.com>
2023-11-12 17:58:17 +0900

    프로젝트 세팅 - Querydsl 라이브러리 추가, JPA, DB 연결

--- Query DSL/querydsl/src/main/java/study/querydsl/QuerydslApplication.java
+++ Query DSL/querydsl/src/main/java/study/querydsl/QuerydslApplication.java
@@ -1,9 +1,14 @@
 package study.querydsl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
+@Transactional
 public class QuerydslApplication {
 
 	public static void main(String[] args) {
